ed5238b8e0efbe99320193937f1c5189e72d0ee7
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-03-17 16:28:55 -0700

    Make it export a zip file

--- convert-all.sh
+++ convert-all.sh
@@ -1,4 +1,12 @@
+echo 'Clearing existing emojis'
+mkdir -p out/
+rm out/*.png
+
 ls flags/*.png | xargs basename -s .png | xargs -n1 ./create-emojis.sh
 echo 'Created emojis: '
 ls out/*.png | xargs basename -s .png | sed 's/.*/\:&\:/' | tr '\n' ' ' | xargs echo
+
+EXPORT_ZIP=emojis.zip
+zip $EXPORT_ZIP out
+echo 'Created zip file' $EXPORT_ZIP
 
