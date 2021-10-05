1a06f1e94f724e487efd332297037f40b3d34011
Astrid Yu <astrid@astrid.tech>
2021-10-05 10:47:46 -0700

    Add sparkles and eggs

--- convert-all.sh
+++ convert-all.sh
@@ -6,6 +6,11 @@ ls flags/*.png | xargs basename -s .png | xargs -n1 ./create-emojis.sh
 echo 'Created emojis: '
 ls out/*.png | xargs basename -s .png | sed 's/.*/\:&\:/' | tr '\n' ' ' | xargs echo
 
+for emoji in out/*.png; do
+	echo 'Sparkling' $emoji
+	composite -compose Over sparkle.png "$emoji" "out/$(basename -s .png $emoji).sparkle.png"
+done
+
 EXPORT_ZIP=emojis.zip
 zip $EXPORT_ZIP out/*.png
 echo 'Created zip file' $EXPORT_ZIP
--- create-emojis.sh
+++ create-emojis.sh
@@ -4,33 +4,28 @@ NAME="$1"
 OUT_DIR="out"
 
 IN="flags/$NAME.png"
-OUT_FLAG_TMP="/tmp/${NAME}_flag_tmp.png"
+IN_SCL="/tmp/pride-flag-emoji-rescaled/$NAME.png"
 OUT_FLAG="$OUT_DIR/${NAME}_flag.png"
-
-OUT_HEART_TMP="/tmp/${NAME}_heart.png"
 OUT_HEART="$OUT_DIR/${NAME}_heart.png"
-
-OUT_MIDDLE_TMP="/tmp/${NAME}_middle.png"
 OUT_MIDDLE="$OUT_DIR/${NAME}_middle.png"
+OUT_EGG="$OUT_DIR/${NAME}_egg.png"
+
+mkdir -p /tmp/pride-flag-emoji-rescaled
 
 echo Using source file $IN
+convert $IN -resize 128x128\! -background transparent -gravity center -extent 128x128 $IN_SCL
 
 echo Generating $OUT_FLAG
-convert $IN -resize x500 -background transparent -gravity center -extent 630x630 $OUT_FLAG_TMP
-composite -compose Dst_In flag_mask.png $OUT_FLAG_TMP -alpha Set $OUT_FLAG_TMP
-convert $OUT_FLAG_TMP -resize 256x256 $OUT_FLAG
-rm $OUT_FLAG_TMP
+convert $IN -resize 128x90\! -background transparent -gravity center -extent 128x128 $OUT_FLAG
+composite -compose Dst_In flag_mask.png $OUT_FLAG -alpha Set $OUT_FLAG
 
 echo Generating $OUT_HEART
-convert $IN -resize x505 -background transparent -gravity center -extent 630x630+0+16 $OUT_HEART_TMP
-composite -compose Dst_In heart_mask.png $OUT_HEART_TMP -alpha Set $OUT_HEART_TMP
-convert $OUT_HEART_TMP -resize 256x256 $OUT_HEART
-rm $OUT_HEART_TMP
+composite -compose Dst_In heart_mask.png $IN_SCL -alpha Set $OUT_HEART
 
 echo Generating $OUT_MIDDLE
-convert $IN -resize x256 -background transparent -gravity center $OUT_MIDDLE_TMP
-composite -compose Dst_In middle_mask.png $OUT_MIDDLE_TMP -alpha Set $OUT_MIDDLE_TMP
-convert $OUT_MIDDLE_TMP -resize 256x256 $OUT_MIDDLE
-rm $OUT_MIDDLE_TMP
+composite -compose Dst_In middle_mask.png $IN_SCL -alpha Set $OUT_MIDDLE
+
+echo Generating $OUT_EGG
+composite -compose Dst_In egg_mask.png $IN_SCL -alpha Set $OUT_EGG
 
 echo Finished processing $NAME
